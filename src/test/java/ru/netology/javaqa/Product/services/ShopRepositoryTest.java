b2fc4f0659cfe86da4ae819b262aa02e4047ca2c
Artem test <artempronajtis@gmail.com>
2024-03-13 18:36:20 +0200

    Product changes

---
+++ src/test/java/ru/netology/javaqa/Product/services/ShopRepositoryTest.java
@@ -0,0 +1,37 @@
+package ru.netology.javaqa.Product.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ShopRepositoryTest {
+    @Test
+    public void testRemoveExistingElement() {
+        ShopRepository repository = new ShopRepository();
+        Product product1 = new Product(1, "Product 1", 100);
+        Product product2 = new Product(2, "Product 2", 200);
+        repository.add(product1);
+        repository.add(product2);
+
+        repository.removeById(1);
+
+        Product[] expectedProducts = {product2};
+        assertArrayEquals(expectedProducts, repository.findAll());
+    }
+
+    @Test
+    public void testRemoveNonExistingElement() {
+        ShopRepository repository = new ShopRepository();
+        Product product1 = new Product(1, "Product 1", 100);
+        Product product2 = new Product(2, "Product 2", 200);
+        repository.add(product1);
+        repository.add(product2);
+
+        try {
+            repository.removeById(3);
+            fail("Expected NotFoundException was not thrown");
+        } catch (NotFoundException e) {
+            assertEquals("Element with id: 3 not found", e.getMessage());
+        }
+    }
+}
